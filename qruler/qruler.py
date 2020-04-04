0dee74e58befa06e2bf240381e741a2237dbff0f
hydrargyrum <dev@indigo.re>
2020-04-04 23:47:25 +0200

    qruler: can change direction of ticks + few more changes
    - add a context menu
    - can change direction of ruler (left-to-right vs right-to-left)
    - can change the edge of ticks
    - can resize with shift+arrow

--- qruler/qruler.py
+++ qruler/qruler.py
@@ -3,9 +3,9 @@
 # license: WTFPL [http://www.wtfpl.net/]
 # A Qt app that just displays a ruler to measure pixel lengths
 
-from PyQt5.QtCore import Qt, QSize, QRect
-from PyQt5.QtGui import QPainter
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtCore import Qt, QSize, QRect, pyqtSlot as Slot
+from PyQt5.QtGui import QPainter, QKeySequence
+from PyQt5.QtWidgets import QApplication, QWidget, QAction
 
 import sys
 
@@ -14,6 +14,8 @@ class RulerWidget(QWidget):
 	def __init__(self, *args):
 		super(RulerWidget, self).__init__(*args)
 		self.orientation = Qt.Horizontal
+		self.firstEdge = False
+		self.inv = False
 
 	def setOrientation(self, orientation):
 		self.orientation = orientation
@@ -21,38 +23,81 @@ class RulerWidget(QWidget):
 
 	def paintEvent(self, event):
 		painter = QPainter(self)
-		
-		if self.orientation == Qt.Horizontal:
-			ticksLineEnd = self.size().height()
-			ticksLast = self.size().width()
-		else:
-			ticksLineEnd = self.size().width()
-			ticksLast = self.size().height()
-			
-		for tick in range(0, ticksLast, 5):
-			if tick % 50 == 0:
-				self._drawTick(painter, tick, ticksLineEnd - 20, ticksLineEnd)
-				self._drawTickLabel(painter, tick, ticksLineEnd - 20)
-			elif tick % 10 == 0:
-				self._drawTick(painter, tick, ticksLineEnd - 10, ticksLineEnd)
-			else:
-				self._drawTick(painter, tick, ticksLineEnd - 5, ticksLineEnd)
 
-	def _drawTick(self, painter, tick, tickStart, tickEnd):
+		tick = 0
+		while True:
+			if tick % 50 == 0:
+				if not self._drawTick(painter, tick, 20):
+					break
+				self._drawTickLabel(painter, tick, 20)
+			elif tick % 10 == 0:
+				if not self._drawTick(painter, tick, 10):
+					break
+			else:
+				if not self._drawTick(painter, tick, 5):
+					break
+
+			tick += 5
+
+	def _drawTick(self, painter, tick, length):
+		if self.firstEdge:
+			tickStart = 0
+			tickEnd = length
+
 		if self.orientation == Qt.Horizontal:
+			if self.inv:
+				tick = self.size().width() - tick - 1
+
+			if not (0 <= tick < self.size().width()):
+				return False
+
+			if not self.firstEdge:
+				tickStart = self.size().height()
+				tickEnd = tickStart - length
+
 			painter.drawLine(tick, tickStart, tick, tickEnd)
 		else:
+			if self.inv:
+				tick = self.size().height() - tick - 1
+
+			if not (0 <= tick < self.size().height()):
+				return False
+
+			if not self.firstEdge:
+				tickStart = self.size().width()
+				tickEnd = tickStart - length
+
 			painter.drawLine(tickStart, tick, tickEnd, tick)
 
-	def _drawTickLabel(self, painter, tick, tickStart):
+		return True
+
+	def _drawTickLabel(self, painter, tick, pos):
+		label = str(tick)
+
 		if self.orientation == Qt.Horizontal:
+			if self.inv:
+				tick = self.size().width() - tick - 1
+
+			if self.firstEdge:
+				tickStart = pos + 10
+			else:
+				tickStart = self.size().height() - pos - 10
+
 			textFlags = Qt.TextDontClip | Qt.AlignBottom | Qt.AlignHCenter
-			rect = QRect(tick - 10, tickStart - 10, 20, 10)
-			painter.drawText(rect, textFlags, str(tick))
+			rect = QRect(tick - 10, tickStart, 20, 10)
+			painter.drawText(rect, textFlags, label)
 		else:
+			if self.inv:
+				tick = self.size().height() - tick - 1
+
+			if self.firstEdge:
+				tickStart = pos + 20
+			else:
+				tickStart = self.size().width() - pos - 20
+
 			textFlags = Qt.TextDontClip | Qt.AlignRight | Qt.AlignVCenter
-			rect = QRect(tickStart - 20, tick - 5, 20, 10)
-			painter.drawText(rect, textFlags, str(tick))
+			rect = QRect(tickStart, tick - 5, 20, 10)
+			painter.drawText(rect, textFlags, label)
 
 	def sizeHint(self):
 		if self.orientation == Qt.Horizontal:
@@ -64,34 +109,91 @@ class RulerWidget(QWidget):
 class RulerWindow(RulerWidget):
 	def __init__(self, *args):
 		super(RulerWindow, self).__init__(*args)
-		self.setToolTip('Double-click to toggle horizontal/vertical.\n'
-		                'Press arrows to move window by 1 pixel.\n'
-		                'Press B to toggle borderless.'
+		self.setToolTip(
+			'Double-click (or press V) to toggle horizontal/vertical.\n'
+			'Press arrows to move window by 1 pixel.\n'
+			'Press B to toggle borderless.\n'
+			'Press R to toggle reverse direction of the ruler.'
 		)
 		self.setWindowFlags(self.windowFlags() | Qt.WindowStaysOnTopHint)
 
+		self.vertAction = QAction('&Vertical', self)
+		self.vertAction.setShortcut(QKeySequence('v'))
+		self.vertAction.setCheckable(True)
+		self.vertAction.triggered.connect(self.toggleVertical)
+		self.addAction(self.vertAction)
+
+		self.borderAction = QAction('&Borderless', self)
+		self.borderAction.setShortcut(QKeySequence('b'))
+		self.borderAction.setCheckable(True)
+		self.borderAction.triggered.connect(self.toggleBorderless)
+		self.addAction(self.borderAction)
+
+		self.edgeAction = QAction('First edge', self)
+		self.edgeAction.setCheckable(True)
+		self.edgeAction.triggered.connect(self.toggleEdge)
+		self.addAction(self.edgeAction)
+
+		self.invAction = QAction('&Reverse direction', self)
+		self.invAction.setShortcut(QKeySequence('r'))
+		self.invAction.setCheckable(True)
+		self.invAction.triggered.connect(self.toggleInv)
+		self.addAction(self.invAction)
+
+		self.setContextMenuPolicy(Qt.ActionsContextMenu)
+
 	def keyPressEvent(self, event):
+		xy = None
 		if event.key() == Qt.Key_Left:
-			self.move(self.x() - 1, self.y())
+			xy = -1, 0
 		elif event.key() == Qt.Key_Right:
-			self.move(self.x() + 1, self.y())
+			xy = 1, 0
 		elif event.key() == Qt.Key_Up:
-			self.move(self.x(), self.y() - 1)
+			xy = 0, -1
 		elif event.key() == Qt.Key_Down:
-			self.move(self.x(), self.y() + 1)
-		elif event.key() == Qt.Key_B:
-			self.hide()  # seems the flag can't be set when window is visible
-			self.setWindowFlags(self.windowFlags() ^ Qt.FramelessWindowHint)
-			self.show()
-		else:
+			xy = 0, 1
+
+		if xy is None:
 			super(RulerWindow, self).keyPressEvent(event)
+		else:
+			if event.modifiers() & Qt.ShiftModifier:
+				self.resize(self.width() + xy[0], self.height() + xy[1])
+			else:
+				self.move(self.x() + xy[0], self.y() + xy[1])
 
 	def mouseDoubleClickEvent(self, event):
+		self.toggleVertical()
+
+	@Slot()
+	def toggleVertical(self):
 		if self.orientation == Qt.Horizontal:
 			self.setOrientation(Qt.Vertical)
+			self.vertAction.setChecked(True)
 		else:
 			self.setOrientation(Qt.Horizontal)
+			self.vertAction.setChecked(False)
 		self.resize(self.height(), self.width()) # transpose size
+
+	@Slot()
+	def toggleBorderless(self):
+		size = self.size()
+
+		self.hide()  # seems the flag can't be set when window is visible
+		self.setWindowFlags(self.windowFlags() ^ Qt.FramelessWindowHint)
+		self.borderAction.setChecked(self.windowFlags() & Qt.FramelessWindowHint)
+		self.show()
+
+		self.resize(size)
+
+	@Slot()
+	def toggleEdge(self):
+		self.firstEdge = not self.firstEdge
+		self.update()
+
+	@Slot()
+	def toggleInv(self):
+		self.inv = not self.inv
+		self.update()
 
 
 def main():
