f552801e8321035aba5ef12d8089bbc1c43b5f85
hydrargyrum <dev@indigo.re>
2022-11-28 22:19:53 +0100

    crc: add tests

---
+++ crc/test.sh
@@ -0,0 +1,15 @@
+#!/bin/sh -eu
+
+cd "$(dirname "$0")"
+
+assertequals () {
+	if [ "$1" != "$2" ]
+	then
+		printf "Expected: %s\nGot: %s\n" "$1" "$2"
+		exit 1
+	fi
+}
+
+assertequals "9ef61f95" "$(printf foobar | ./crc.py --hex -4)"
+assertequals "b025" "$(printf foobar | ./crc.py --hex -2)"
+
