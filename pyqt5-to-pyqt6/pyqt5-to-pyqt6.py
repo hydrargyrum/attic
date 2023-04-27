1763e6e6efe4750f9c3036bcbb5cfc4e61dcd880
hydrargyrum <dev@indigo.re>
2023-04-27 22:44:10 +0200

    pyqt5-to-pyqt6: don't play cat and mouse, discover entire qt modules

--- pyqt5-to-pyqt6/pyqt5-to-pyqt6.py
+++ pyqt5-to-pyqt6/pyqt5-to-pyqt6.py
@@ -2,50 +2,67 @@
 # SPDX-License-Identifier: WTFPL
 
 import argparse
+import enum
 import re
 
-from PyQt6.QtCore import Qt
-from PyQt6.QtGui import QKeySequence, QClipboard
-from PyQt6.QtWidgets import (
-    QSizePolicy, QStyle, QRubberBand, QDialogButtonBox, QDialog, QMessageBox,
-)
+import PyQt6.QtCore
+import PyQt6.QtGui
+import PyQt6.QtWidgets
 
 
 REPLACEMENTS = {}
 
 
 def process_obj(obj):
-    REPLACEMENTS[obj.__name__] = {
+    try:
+        obj_name = obj.__name__
+    except AttributeError:
+        return
+
+    REPLACEMENTS[obj_name] = {
         entry.name: f"{enum_name}.{entry.name}"
         for enum_name in dir(obj)
         if enum_name[0].isupper()
-        for entry in getattr(obj, enum_name)
+        for sub_obj in (getattr(obj, enum_name),)  # XXX hack to emulate walrus
+        if isinstance(sub_obj, type) and issubclass(sub_obj, enum.Enum)
+        for entry in sub_obj
     }
+
+
+def process_module(module):
+    for attr in dir(module):
+        obj = getattr(module, attr)
+        process_obj(obj)
 
 
 def main():
     parser = argparse.ArgumentParser(
         description="Dumb port FILE from PyQt5 to PyQt6, such as enums, exec_, etc."
     )
+    parser.add_argument("-d", action="store_true")
     parser.add_argument("file")
     args = parser.parse_args()
 
-    process_obj(Qt)
-    process_obj(QKeySequence)
-    process_obj(QClipboard)
-    process_obj(QSizePolicy)
-    process_obj(QStyle)
-    process_obj(QRubberBand)
-    process_obj(QDialog)
-    process_obj(QDialogButtonBox)
-    process_obj(QMessageBox)
+    process_module(PyQt6.QtCore)
+    process_module(PyQt6.QtGui)
+    process_module(PyQt6.QtWidgets)
+
+    if args.d:
+        import json; print(json.dumps(REPLACEMENTS, indent=2))
 
     with open(args.file) as fp:
         text = fp.read()
 
+    new_replace = {}
     for obj_name, obj_replaces in REPLACEMENTS.items():
         for old, new in obj_replaces.items():
-            text = re.sub(fr"\b{obj_name}\.{old}\b", fr"{obj_name}.{new}", text)
+            new_replace[f"{obj_name}.{old}"] = f"{obj_name}.{new}"
+
+    def do_replace(match):
+        return new_replace[match[0]]
+
+    bigsearch = "|".join(re.escape(pattern) for pattern in new_replace)
+    text = re.sub(fr"\b(?:{bigsearch})\b", do_replace, text)
 
     text = re.sub(r"\bPyQt5\b", "PyQt6", text)
     text = re.sub(r"\bexec_\(", "exec(", text)
