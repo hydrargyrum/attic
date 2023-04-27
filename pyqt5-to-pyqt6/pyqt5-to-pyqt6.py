ac1bb0a57a499a118940be460f9ff208e3994457
hydrargyrum <dev@indigo.re>
2023-04-27 22:30:32 +0200

    pyqt5-to-pyqt6: more fixes

--- pyqt5-to-pyqt6/pyqt5-to-pyqt6.py
+++ pyqt5-to-pyqt6/pyqt5-to-pyqt6.py
@@ -5,8 +5,10 @@ import argparse
 import re
 
 from PyQt6.QtCore import Qt
-from PyQt6.QtGui import QKeySequence
-from PyQt6.QtWidgets import QSizePolicy
+from PyQt6.QtGui import QKeySequence, QClipboard
+from PyQt6.QtWidgets import (
+    QSizePolicy, QStyle, QRubberBand, QDialogButtonBox, QDialog, QMessageBox,
+)
 
 
 REPLACEMENTS = {}
@@ -30,7 +32,13 @@ def main():
 
     process_obj(Qt)
     process_obj(QKeySequence)
+    process_obj(QClipboard)
     process_obj(QSizePolicy)
+    process_obj(QStyle)
+    process_obj(QRubberBand)
+    process_obj(QDialog)
+    process_obj(QDialogButtonBox)
+    process_obj(QMessageBox)
 
     with open(args.file) as fp:
         text = fp.read()
@@ -40,7 +48,15 @@ def main():
             text = re.sub(fr"\b{obj_name}\.{old}\b", fr"{obj_name}.{new}", text)
 
     text = re.sub(r"\bPyQt5\b", "PyQt6", text)
-    text = re.sub(r"\bexec_\(\b", "exec(", text)
+    text = re.sub(r"\bexec_\(", "exec(", text)
+
+    m = re.search("mouse(?:Press|Move|Release)Event", text)
+    if m:
+        print(f"warning: {m[0]} is used, QMouseEvent interface changed")
+    if "QAction" in text:
+        print("warning: QAction moved to PyQt6.QtGui")
+    if "qApp" in text:
+        print("warning: qApp has been removed")
 
     with open(args.file, "w") as fp:
         fp.write(text)
