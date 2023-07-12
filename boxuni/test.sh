be71cd82706854a3791aa255d2d8fc36bf11d0eb
hydrargyrum <dev@indigo.re>
2023-07-12 21:51:14 +0200

    boxuni: add test.sh

---
+++ boxuni/test.sh
@@ -0,0 +1,40 @@
+#!/bin/sh -eu
+# shellcheck enable=
+
+cd "$(dirname "$0")"
+
+got=$(mktemp sorted.XXXXXX)
+trap 'rm "$got"' EXIT
+
+
+init () {
+	./boxuni > "$got"
+}
+
+check () {
+	diff -u - "$got"
+}
+
+
+# basic test
+init <<- EOF
+	+----------+--------+
+	| name     | color  |
+	+----------+--------+
+	| zucchini | green  |
+	| tomato   | red    |
+	| banana   | yellow |
+	| orange   | orange |
+	+----------+--------+
+EOF
+
+check <<- EOF
+	┌──────────┬────────┐
+	│ name     │ color  │
+	├──────────┼────────┤
+	│ zucchini │ green  │
+	│ tomato   │ red    │
+	│ banana   │ yellow │
+	│ orange   │ orange │
+	└──────────┴────────┘
+EOF
