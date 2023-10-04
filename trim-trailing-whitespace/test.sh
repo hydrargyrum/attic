7243ecb4c556494d49764a16d294999cc72da03c
hydrargyrum <dev@indigo.re>
2023-10-04 11:23:55 +0200

    trim-trailing-whitespace: add test.sh

---
+++ trim-trailing-whitespace/test.sh
@@ -0,0 +1,22 @@
+#!/bin/sh -eu
+
+cd "$(dirname "$0")"
+
+got=$(mktemp sorted.XXXXXX)
+trap 'rm "$got"' EXIT
+
+init () {
+	./trim-trailing-whitespace "$@" > "$got"
+}
+
+check () {
+	diff -u - "$got"
+}
+
+printf "foo\n\n  b a r\n baz   \t\nqux \n" | init
+printf "foo\n\n  b a r\n baz\nqux\n" | check
+
+# test without newline at EOF
+printf "foo\n\n  b a r\n baz   \t\nqux " | init
+printf "foo\n\n  b a r\n baz\nqux" | check
+
