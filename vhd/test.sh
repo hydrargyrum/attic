a75b909729574f9042ac1505a9ff6791a2679362
hydrargyrum <dev@indigo.re>
2024-01-05 00:00:00 +0100

    vhd: add test.sh

---
+++ vhd/test.sh
@@ -0,0 +1,51 @@
+#!/bin/sh -eu
+# SPDX-License-Identifier: WTFPL
+# shellcheck enable=
+
+cd "$(dirname "$0")"
+
+got=$(mktemp vhd.XXXXXX)
+trap 'rm "$got"' EXIT
+
+
+init () {
+	./vhd "$@" > "$got"
+}
+
+check () {
+	diff -u - "$got"
+}
+
+
+# basic test
+init <<- EOF
+	foo bar
+	baz é
+EOF
+
+check <<- EOF
+	f  o  o  sp b  a  r  \n
+	b  a  z  sp C3 A9 \n
+EOF
+
+# lines
+init -L <<- EOF
+	foo bar
+	baz é
+EOF
+
+check <<- EOF
+	00000001 f  o  o  sp b  a  r  \n
+	00000002 b  a  z  sp C3 A9 \n
+EOF
+
+# hex lines
+init --line-addresses <<- EOF
+	foo bar
+	baz é
+EOF
+
+check <<- EOF
+	00000000 f  o  o  sp b  a  r  \n
+	00000008 b  a  z  sp C3 A9 \n
+EOF
