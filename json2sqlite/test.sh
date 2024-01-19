58a9484940abed7653e208dde26a1c0096619dbc
hydrargyrum <dev@indigo.re>
2024-01-19 12:35:00 +0100

    json2sqlite: repair broken --update and add tests

--- json2sqlite/test.sh
+++ json2sqlite/test.sh
@@ -4,20 +4,78 @@
 
 cd "$(dirname "$0")"
 
+TRUNC=
 got=$(mktemp json2sqlite.XXXXXX)
-trap 'rm "$got"' EXIT
+csv=$(mktemp json2sqlite.XXXXXX)
+trap 'rm "$got" "$csv"' EXIT
 
+trunc () {
+	: > "$got"
+}
 
-init () {
-	./json2sqlite "$@" -d "$got"
+run () {
+	[ -n "${TRUNC-}" ] && trunc
+	./json2sqlite -d "$got" "$@"
 }
 
 check () {
-	test -f "$got"
+	sqlite3 -csv "$got" -header "$@" > "$csv"
+	diff -u - "$csv"
 }
 
+# basic test input as list[dict]
+TRUNC=y run -f - -t test --create <<- EOF
+	[{"col1": "1.1", "col2": "1.2"}, {"col1": "2.1", "col2": "2.2"}]
+EOF
 
-# basic test
-init -f - -t test --create <<- EOF
-	[{"col1": "1,1", "col2": "2,1"}, {"col1": "1,2", "col2": "2,2"}]
+check "select * from test" <<- EOF
+	col1,col2
+	1.1,1.2
+	2.1,2.2
+EOF
+
+# basic test input as list[dict] as arguments
+TRUNC=y run -t test --create -j '[{"col1": "1.1", "col2": "1.2"}, {"col1": "2.1", "col2": "2.2"}]'
+
+check "select * from test" <<- EOF
+	col1,col2
+	1.1,1.2
+	2.1,2.2
+EOF
+
+# test input as dict (single entry)
+TRUNC=y run -f - -t test --create <<- EOF
+	{"col1": "1.1", "col2": "1.2"}
+EOF
+
+check "select * from test" <<- EOF
+	col1,col2
+	1.1,1.2
+EOF
+
+# test input as list[list]
+trunc
+sqlite3 "$got" "create table test(foo, bar)"
+run -f - -t test --create <<- EOF
+	[["1.1", "1.2"], ["2.1", "2.2"]]
+EOF
+
+check "select * from test" <<- EOF
+	foo,bar
+	1.1,1.2
+	2.1,2.2
+EOF
+
+# test --update
+TRUNC=y run -f - -t test --create <<- EOF
+	[{"col1": "1.1", "col2": "1.2"}, {"col1": "2.1", "col2": "2.2"}]
+EOF
+run --file - --table test --update col1 <<- EOF
+	[{"col1": "1.1", "col2": "new1"}, {"col1": "2.1", "col2": "new2"}]
+EOF
+
+check "select * from test" <<- EOF
+	col1,col2
+	1.1,new1
+	2.1,new2
 EOF
